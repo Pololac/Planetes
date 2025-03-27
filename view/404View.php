44c37b40d5b306b04d94762a59d8a909f3b28c88
Paul Lacoste <lacostepaul@yahoo.fr>
2025-03-27 18:07:34 +0100

    Page 404 : mise en forme avec visuel

--- view/404View.php
+++ view/404View.php
@@ -3,23 +3,29 @@
 // Vue de la page 404
 // --------------------------------
 $titre = "Erreur 404";
-include "include/header.php";
+include_once "includes/header.php";
 
 ?>
 
-<body>
-    <div class="container mt-5 mb-5">
-        <a href="?" class="btn btn-secondary mb-2">Revenir à l'accueil</a>
+<body class="min-vh-100 d-flex flex-column">
+    <div class="container mt-5 mb-5 text-center" style="height: 600px;">
+        <div class="d-flex justify-content-end">
+            <a href="?" class="btn btn-secondary">Revenir à l'accueil</a>
+        </div>
 
-        <h2><?=$titre?></h2>
-
-        <p>Veuillez nous excuser mais la page que vous demandez n'existe pas</p>
+        <div class="mt-5">
+            <h2><?=$titre?></h2>
+            <p class="mt-4">Veuillez nous excuser mais la page que vous demandez n'existe pas.</p>
+            <img src="assets/images/page-lost.svg" alt="Une soucoupe d'extraterrestres" width="500" height="500">
+        </div>
+        
     </div>
 </body>
+
 </html>
 
 <?php
 
-include "include/footer.php";
+include_once "includes/footer.php";
 
 ?>
