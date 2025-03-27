d29151d4474ff144a02134ea53a4e846623eb12f
Paul Lacoste <lacostepaul@yahoo.fr>
2025-03-27 18:08:45 +0100

    Index (routeur) : fix bug

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // --------------------------------
 // Routeur
@@ -7,13 +7,13 @@
 if (!empty($_GET['page'])) {       // Envoie vers les différents controleurs en fonction de l'info "page"
 
     if ($_GET['page'] == 'planet'){
-        require_once('controller/planetController.php');
+        require_once 'controller/planetController.php';
 
     } else { 
-        require_once('view/404View.php');
+        require_once 'view/404View.php';
     }
 
 } else {
-    require_once('view/homeView.php');
+    require_once 'view/homeView.php';
 }
 
