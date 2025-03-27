ac3970590c3af33be7a05f094c6a9aabc76fd662
Paul Lacoste <lacostepaul@yahoo.fr>
2025-03-27 21:35:20 +0100

    connexion : nettoyage fichier

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,12 +1,5 @@
 <?php
 
-// try {
-//     $db = new PDO('mysql:host=localhost;charset=utf8;dbname=cours_pdo', 'root', 'test');
-// } catch (PDOException $e) {
-//    echo 'Erreur de connexion à la bdd :' .$e->getMessage(). '<br>';
-// }
-
-
 try {
 
     $db = new PDO('mysql:host=localhost;charset=utf8;dbname=hb_r6_php_pdo_exam', 'php', 'PHPcestSuper!');
@@ -16,5 +9,3 @@ try {
     die('Erreur de connexion à la bdd');    // "die" tue la page pour éviter d'exécuter les autres méthodes si la connexion à la BDD n'a pas marché & pas "$e->getMessage()" pour ne pas révéler le mot de passe renseigné
 
 }
-
-// var_dump($db);
