6bcb50ebd963383bd691a742bf900eaba0dbe09c
Pololac <lacostepaul@yahoo.fr>
2025-06-19 22:56:22 +0200

    Faille sécu avec MdP visible dans GitHub : creation fichier config.ini & ajout au .gitignore

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,11 +1,13 @@
 <?php
 
+$config = parse_ini_file(__DIR__ . '/config.ini');
+
 try {
-
-    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=hb_r6_php_pdo_exam', 'php', 'PHPcestSuper!');
-
+    $db = new PDO(
+        'mysql:host=' . $config['DB_HOST'] . ';port=' . $config['DB_PORT'] . ';charset=utf8;dbname=' . $config['DB_NAME'],
+        $config['DB_USER'],
+        $config['DB_PASSWORD']
+    );
 } catch (PDOException) {
-
-    die('Erreur de connexion à la bdd');    // "die" tue la page pour éviter d'exécuter les autres méthodes si la connexion à la BDD n'a pas marché & pas "$e->getMessage()" pour ne pas révéler le mot de passe renseigné
-
+    die('Erreur de connexion à la BDD');
 }
